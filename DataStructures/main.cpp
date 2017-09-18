68fbccdb67d58acc60632f042119629988841fcd
Oles Pankiv <ojlesua@gmail.com>
2017-09-18 22:14:44 +0300

    Removed VStudio files, merged two projects (LinkedList and BinaryTree) into one - DataStructures

---
+++ DataStructures/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+
+#include "BinaryTree.h"
+
+using namespace std;
+
+int main()
+{
+	BinaryTree<int>* tree = new BinaryTree<int>();
+	tree->add(8);
+	tree->add(4);
+	tree->add(10);
+	tree->add(2);
+	tree->add(6);
+	tree->add(9);
+	tree->add(11);
+	tree->add(5);
+	tree->add(7);
+
+	tree->prettyPrint(cout);
+
+	_getch();
+	return 0;
+}
