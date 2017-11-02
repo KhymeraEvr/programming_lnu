4aa0faba7e1acb60050b6a82b2ce77c3cac85944
Oles Pankiv <ojlesua@gmail.com>
2017-11-02 21:26:36 +0200

    Added virtual machine to ProgrammingLanguage project

---
+++ ProgrammingLanguage/VirtualMachine.cpp
@@ -0,0 +1,76 @@
+#include "VirtualMachine.h"
+
+bool isNumber(string&);
+
+void VirtualMachine::run(string code)
+{
+	parseCommands(code);
+	executeSteps();
+}
+
+void VirtualMachine::parseCommands(string& code)
+{
+	stringstream codeStream(code);
+
+	string command;
+	vector<string> parameters;
+
+	while (codeStream >> command)
+	{
+		string param, paramLine;
+		getline(codeStream, paramLine, '\n');
+
+		stringstream paramStream(paramLine);
+		while (paramStream >> param)
+		{
+			parameters.push_back(param);
+		}
+
+		pair<string, vector<string>> step(command, parameters);
+		steps.push_back(step);
+
+		parameters.clear();
+	}
+}
+
+void VirtualMachine::executeSteps()
+{
+	while (stepIndex != steps.size())
+	{
+		functions[steps[stepIndex].first](steps[stepIndex].second);
+		stepIndex++;
+	}
+}
+
+double VirtualMachine::getVariable(string variable)
+{
+	if (isNumber(variable))
+	{
+		return stod(variable);
+	}
+	else
+	{
+		auto variableIterator = variables.find(variable);
+		if (variableIterator == variables.end())
+		{
+			throw invalid_argument("Invalid variable!");
+		}
+		else
+		{
+			return (*variableIterator).second;
+		}
+	}
+}
+
+bool isNumber(string& value)
+{
+	for (char symbol : value)
+	{
+		if ((symbol < '0' || symbol > '9') && symbol != '.')
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
---
+++ ProgrammingLanguage/VirtualMachine.h
@@ -0,0 +1,130 @@
+#ifndef PROGRAMMINGLANGUAGE_VIRTUALMACHINE_H
+#define PROGRAMMINGLANGUAGE_VIRTUALMACHINE_H
+
+#include <functional>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class VirtualMachine
+{
+private:
+	map<string, double> variables;
+
+	vector<pair<string, vector<string>>> steps;
+	int stepIndex = 0;
+
+	map<string, function<void(vector<string>)>> functions =
+			{
+					{
+							"READ",
+							[this](vector<string> params)
+							{
+								double variable;
+								cin >> variable;
+								variables[params[0]] = variable;
+							}
+					},
+					{
+							"WRITE",
+							[this](vector<string> params)
+							{
+								// not sure how to write param
+								// write or writeln?
+								cout << getVariable(params[0]) << endl;
+							}
+					},
+					{
+							"ADD",
+							[this](vector<string> params)
+							{
+								double a = getVariable(params[0]);
+								double b = getVariable(params[1]);
+								variables[params[2]] = a + b;
+							}
+					},
+					{
+							"SUB",
+							[this](vector<string> params)
+							{
+								double a = getVariable(params[0]);
+								double b = getVariable(params[1]);
+								variables[params[2]] = a - b;
+							}
+					},
+					{
+							"DIV",
+							[this](vector<string> params)
+							{
+								double a = getVariable(params[0]);
+								double b = getVariable(params[1]);
+								variables[params[2]] = a / b;
+							}
+					},
+					{
+							"MUL",
+							[this](vector<string> params)
+							{
+								double a = getVariable(params[0]);
+								double b = getVariable(params[1]);
+								variables[params[2]] = a * b;
+							}
+					},
+					{
+							"GOTO",
+							[this](vector<string> params)
+							{
+								// not sure if steps are indexed from zero or one
+								stepIndex = stoi(params[0]) - 2;
+							}
+					},
+					{
+							"GOTOIF",
+							[this](vector<string> params)
+							{
+								double variable = getVariable(params[0]);
+								if (variable != 0)
+								{
+									stepIndex = stoi(params[1]) - 2;
+								}
+							}
+					},
+					{
+							"GOTOIFNOT",
+							[this](vector<string> params)
+							{
+								double variable = getVariable(params[0]);
+								if (variable == 0)
+								{
+									stepIndex = stoi(params[1]) - 2;
+								}
+							}
+					},
+					{
+							"COPY",
+							[this](vector<string> params)
+							{
+								double value = getVariable(params[0]);
+								variables[params[1]] = value;
+							}
+					}
+			};
+
+
+	void parseCommands(string&);
+
+	void executeSteps();
+
+	double getVariable(string);
+
+public:
+	void run(string);
+};
+
+
+#endif
